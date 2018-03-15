6706c21be9a0558b567aee18f17c2af917157898
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-03-14 23:12:58 -0400

    Update Diabetes_analytics.py

--- Diabetes_analytics.py
+++ Diabetes_analytics.py
@@ -3,7 +3,8 @@
 
 # Project Objective:
 # 
-# This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases. The objective is to predict based on diagnostic measurements whether a patient has diabetes.
+# This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases. 
+#The objective is to predict based on diagnostic measurements whether a patient has diabetes.
 # 
 # Dataset Reference:
 # 
